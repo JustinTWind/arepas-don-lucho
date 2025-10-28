081fcbaec3585ae6a7a00fada9eb57f3959b3f39
dilan.villada <dilanvm24@>
2025-10-28 07:01:29 -0500

    Se crea tabla de detalle entrega y relacion

--- src/main/java/com/example/arepas_don_lucho/models/DetalleEntrega.java
+++ src/main/java/com/example/arepas_don_lucho/models/DetalleEntrega.java
@@ -1,4 +1,32 @@
 package com.example.arepas_don_lucho.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "detalle_entrega")
 public class DetalleEntrega {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalTime horaSalidaCocina;
+    private LocalTime horaEntregaCliente;
+    private Boolean estado;
+    private String fotoPrueba;
+    private Byte intentoFallido;
+
+    @OneToOne
+    @JoinColumn(name = "fk_id",referencedColumnName = "id")
+
+    private Pedido pedido;
+
 }
